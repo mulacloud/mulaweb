363fe6587330baee0faeabb5151e2575f1b61ac4
bramandityo <freez.meinster@gmail.com>
2020-02-25 03:16:24 +0700

    first

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Navbar from "./Components/Navbar/Navbar.svelte";
+  import Banner from "./Components/Banner/Banner.svelte";
+  import Services from "./Components/Services/Services.svelte";
+  import About from "./Components/About/About.svelte";
+  import Testimonials from "./Components/Testimonials/Testimonials.svelte";
+  import Footer from "./Components/Footer/Footer.svelte";
+  import DATA from "./Data/data";
+  console.log(DATA);
+</script>
+
+<!-- Navbar -->
+<Navbar navlists={DATA.NAVBAR_DATA} header={DATA.HEADER} />
+<!-- Banner -->
+<Banner bannerData={DATA.BANNER_DATA} } />
+<!-- Services -->
+<Services serviceData={DATA.SERVICE_DATA} />
+<!-- About -->
+<About aboutData={DATA.ABOUT_DATA} />
+<!-- Testimonials -->
+<Testimonials testimonialData={DATA.TECH_DATA} />
+<!-- Footer -->
+<Footer footerData={DATA.FOOTER_DATA} header={DATA.HEADER} />
---
+++ src/Data/data.js
@@ -0,0 +1,114 @@
+const HEADER = "Mula Cloud Project";
+
+const NAVBAR_DATA = [
+  { id: 1, url: "/", label: "Beranda" },
+  { id: 2, url: "#services", label: "Layanan" },
+  { id: 3, url: "#about-us", label: "Tentang Kami" },
+  { id: 4, url: "#testimonials", label: "Teknologi" },
+  { id: 5, url: "#footer", label: "Kontak" }
+];
+const BANNER_DATA = {
+  HEADING: "Bersama membangun hari",
+  DECRIPTION:
+    "Kami membantu mengembangkan aplikasi busnis untuk hari-hari anda yang lebih baik",
+};
+const SERVICE_DATA = {
+  HEADING: "Layanan Kami",
+  ALL_SERVICES: "All Services",
+  SERVICE_LIST: [
+    {
+      LABEL: "Search Engine Optimisation",
+      DESCRIPTION:
+        "To customise the content, technical functionality and scope of your website so that your pages show for a specific set of keyword at the top of a search engine list. In the end, the goal is to attract traffic to your website when they are searching for goods, services or business-related information.",
+      URL: "images/service1.png"
+    },
+    {
+      LABEL: "Content Marketing Strategy",
+      DESCRIPTION:
+        "It is tough but well worth the effort to create clever material that is not promotional in nature, but rather educates and inspires. It lets them see you as a reliable source of information by delivering content that is meaningful to your audience.",
+      URL: "images/service2.png"
+    },
+    {
+      LABEL: "Develop Social Media Strategy",
+      DESCRIPTION:
+        "Many People rely on social networks to discover, research, and educate themselves about a brand before engaging with that organization. The more your audience wants to engage with your content, the more likely it is that they will want to share it.",
+      URL: "images/service3.png"
+    }
+  ]
+};
+
+const ABOUT_DATA = {
+  HEADING: "Why choose us?",
+  TITLE: "Why we're different",
+  IMAGE_URL: "images/network.png",
+  WHY_CHOOSE_US_LIST: [
+    "We provides Cost-Effective Digital Marketing than Others.",
+    "High customer statisfaction and experience.",
+    "Marketing efficiency and quick time to value.",
+    "Clear & transparent fee structure.",
+    "We provides Marketing automation which is an integral platform that ties all of your digital marketing together.",
+    "A strong desire to establish long lasting business partnerships.",
+    "Provide digital marketing to mobile consumer.",
+    "We provides wide range to services in reasonable prices"
+  ]
+};
+const TECH_DATA = {
+  HEADING: "Teknologi yang kami gunakan",
+  TECH_LIST: [
+    {
+      LABEL: "Search Engine Optimisation",
+      DESCRIPTION:
+        "To customise the content, technical functionality and scope of your website so that your pages show for a specific set of keyword at the top of a search engine list. In the end, the goal is to attract traffic to your website when they are searching for goods, services or business-related information.",
+      URL: "images/service1.png"
+    },
+    {
+      LABEL: "Content Marketing Strategy",
+      DESCRIPTION:
+        "It is tough but well worth the effort to create clever material that is not promotional in nature, but rather educates and inspires. It lets them see you as a reliable source of information by delivering content that is meaningful to your audience.",
+      URL: "images/service2.png"
+    },
+    {
+      LABEL: "Develop Social Media Strategy",
+      DESCRIPTION:
+        "Many People rely on social networks to discover, research, and educate themselves about a brand before engaging with that organization. The more your audience wants to engage with your content, the more likely it is that they will want to share it.",
+      URL: "images/service3.png"
+    }
+  ]
+};
+
+const SOCIAL_DATA = {
+  HEADING: "Find us on social media",
+  IMAGES_LIST: [
+    "images/facebook-icon.png",
+    "images/instagram-icon.png",
+    "images/whatsapp-icon.png",
+    "images/twitter-icon.png",
+    "images/linkedin-icon.png",
+    "images/snapchat-icon.png"
+  ]
+};
+
+const FOOTER_DATA = {
+  DESCRIPTION:
+    "We are typically focused on result-based maketing in the digital world. Also, we evaluate your brand’s needs and develop a powerful strategy that maximizes profits.",
+  CONTACT_DETAILS: {
+    HEADING: "Contact us",
+    ADDRESS: "La trobe street docklands, Melbourne",
+    MOBILE: "+1 61234567890",
+    EMAIL: "nixalar@gmail.com"
+  },
+  SUBSCRIBE_NEWSLETTER: "Subscribe newsletter",
+  SUBSCRIBE: "Subscribe"
+};
+
+const MOCK_DATA = {
+  HEADER,
+  NAVBAR_DATA,
+  BANNER_DATA,
+  SERVICE_DATA,
+  ABOUT_DATA,
+  TECH_DATA,
+  SOCIAL_DATA,
+  FOOTER_DATA
+};
+export default MOCK_DATA;
